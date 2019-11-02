ba8584f5f702ab5c4394914f62d1f70b5ec4f2a5
johnnyasd12 <johnnnyasd12@gmail.com>
2019-11-02 19:48:15 +0800

    delete timer version 1, add TODO

--- my_utils.py
+++ my_utils.py
@@ -4,6 +4,7 @@ import time
 
 class Timer2:
     def __init__(self, name, enable):
+        # TODO: accumulate the time interval of all iterations
 #         self.name = name
         self.start_t = {}
         self.last_t = {}
@@ -41,23 +42,3 @@ class Timer2:
             print('Duration from "' + self.duration['from'] + '" to "' + self.duration['to'] +'": ')
             print('time:', self.duration['time'], ', process time:', self.duration['process_time'])
         self.last_t = self.end_t.copy()
-
-class Timer:
-    def __init__(self, s):
-        self.name = s
-        self.start = {}
-        self.start['time'] = time.time()
-        self.start['clock'] = time.clock()
-        self.start['process_time'] = time.process_time()
-        self.end = {}
-        self.duration = {}
-        self.duration['name'] = s
-    
-    def __call__(self):
-        self.keys = ['time', 'clock', 'process_time']
-        self.end['time'] = time.time()
-        self.end['clock'] = time.clock()
-        self.end['process_time'] = time.process_time()
-        for name in self.keys:
-            self.duration[name] = self.end[name] - self.start[name]
-        print(self.duration)
